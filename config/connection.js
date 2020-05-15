770e0b6c44b83400ab651df375c9dbf130e6df95
avery rouser <arouser2112@gmai.com>
2020-05-14 20:17:28 -0500

    authentication

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "Rouser12",
+        password: "Kiparis12",
         database: "burgers_db"
     });
 };
